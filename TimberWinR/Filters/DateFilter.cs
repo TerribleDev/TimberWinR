b15f36d33cb3e47e276be595a2e58c81662c0d5f
Jonathan Preddy <jpreddy@vistaprint.com>
2014-07-22 11:22:33 -0400

    Adding properties for DateFilter.

--- TimberWinR/Filters/DateFilter.cs
+++ TimberWinR/Filters/DateFilter.cs
@@ -7,6 +7,11 @@ namespace TimberWinR.Filters
 {
     public class DateFilter : FilterBase
     {
+        public string Field { get; private set; }
+        public string Target { get; private set; }
+        public bool ConvertToUTC { get; private set; }
+        public List<string> Pattern { get; private set; }
+
         public override void Apply(Newtonsoft.Json.Linq.JObject json)
         {
             throw new NotImplementedException();
