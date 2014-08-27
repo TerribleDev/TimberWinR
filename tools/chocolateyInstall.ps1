39c2cbd5d286fdc767858202187296cab86745e2
Eric Fontana <efontana@vistaprint.com>
2014-08-27 11:05:49 -0400

    Update version

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'TimberWinR' # arbitrary name for the package, used in messages
 $installerType = 'msi' #only one of these: exe, msi, msu
-$url = 'http://www.ericfontana.com/TimberWix/TimberWinR-1.2.175.0.msi' # download url
+$url = 'http://www.ericfontana.com/TimberWix/TimberWinR-${version}.0.msi' # download url
 $silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
